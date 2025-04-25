7093d3864967669652cdbb97480c3967b07d91b5
MDTravis <59547361+MDTravisYT@users.noreply.github.com>
2025-04-25 12:36:05 -0400

    organization + art compiler

--- assemble.bat
+++ assemble.bat
@@ -1,4 +1,7 @@
 @echo off
+cd ART
+"../TOOL/mdtiler.exe" makeart
+cd ..
 if not exist OUT mkdir OUT
 "TOOL/vasmm68k_psi-x.exe" -altlocal -m68000 -maxerrors=0 -Fbin -start=0 -o "OUT/MD0425.BIN" -L "OUT/MD0425.LST" -Lall "SRC/MAIN.S" 2> _errors.log
 type _errors.log
